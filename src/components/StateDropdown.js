79b3eec9854e2f2fa1ceda47e3b19e4f3f20d4c5
Christina Holland <hsubox@gmail.com>
2017-03-31 18:00:34 -0700

    Cleaned up state dropdown functionality

--- src/components/StateDropdown.js
+++ src/components/StateDropdown.js
@@ -1,6 +1,10 @@
 import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
 import states from '../data/stateList';
 import '../css/StateDropdown.css';
+
+const BOTTOM_MARGIN = 20;
+const DEFAULT_HEIGHT = 50;
 
 class StateDropdown extends Component {
   constructor() {
@@ -8,10 +12,25 @@ class StateDropdown extends Component {
     this.state = {
       dropdownOpen: false
     };
+    this.handleOutsideClick = this.handleOutsideClick.bind(this);
+  }
+  componentDidMount() {
+    window.addEventListener('click', this.handleOutsideClick);
+  }
+  componentWillUnmount() {
+    window.removeEventListener('click', this.handleOutsideClick);
+  }
+  handleOutsideClick(e) {
+    if (!ReactDOM.findDOMNode(this).contains(e.target)) {
+      this.setState({ dropdownOpen: false });
+    };
   }
   render() {
     return (
-      <div className="state-dropdown">
+      <div
+        className="state-dropdown"
+        ref={node => {if (node) this._bottom = node.getBoundingClientRect().bottom}}
+      >
         <div
           className="state-box selected-state-box"
           onClick={() => this.setState({ dropdownOpen: !this.state.dropdownOpen })}
@@ -20,7 +39,10 @@ class StateDropdown extends Component {
           <div className="arrow-down" />
         </div>
         {this.state.dropdownOpen && (
-          <div className="options-container">
+          <div
+            className="options-container"
+            style={{ height: window.innerHeight - this._bottom - BOTTOM_MARGIN || DEFAULT_HEIGHT }}
+          >
             {states.map(state => (
               <div className="state-box state-option-box" key={state.abbreviation}>
                 {state.name}
